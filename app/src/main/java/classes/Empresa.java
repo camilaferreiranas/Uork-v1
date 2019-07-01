4edf6ad9d85750ddbeb9f553ce94b5e8e817cdc6
Camila Ferreira <camila_fn97@hotmail.com>
2019-07-01 11:41:29 -0300

    Initial commit

---
+++ app/src/main/java/classes/Empresa.java
@@ -0,0 +1,9 @@
+package classes;
+
+public class Empresa {
+    private int id_empresa;
+    private String nome_empresa;
+    private boolean empresa_assinante;
+    private long cnpj_empresa;
+
+}
---
+++ app/src/main/java/classes/Usuario.java
@@ -0,0 +1,67 @@
+package classes;
+
+public class Usuario {
+    private int id_usuario;
+    private long cpf_usuario;
+    private String nomecompleto_usuario;
+    private String area_usuario;
+    private double valorhora_usuario;
+    private int idade_usuario;
+    private String sexo_usuario;
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public long getCpf_usuario() {
+        return cpf_usuario;
+    }
+
+    public void setCpf_usuario(long cpf_usuario) {
+        this.cpf_usuario = cpf_usuario;
+    }
+
+    public String getNomecompleto_usuario() {
+        return nomecompleto_usuario;
+    }
+
+    public void setNomecompleto_usuario(String nomecompleto_usuario) {
+        this.nomecompleto_usuario = nomecompleto_usuario;
+    }
+
+    public String getArea_usuario() {
+        return area_usuario;
+    }
+
+    public void setArea_usuario(String area_usuario) {
+        this.area_usuario = area_usuario;
+    }
+
+    public double getValorhora_usuario() {
+        return valorhora_usuario;
+    }
+
+    public void setValorhora_usuario(double valorhora_usuario) {
+        this.valorhora_usuario = valorhora_usuario;
+    }
+
+    public int getIdade_usuario() {
+        return idade_usuario;
+    }
+
+    public void setIdade_usuario(int idade_usuario) {
+        this.idade_usuario = idade_usuario;
+    }
+
+    public String getSexo_usuario() {
+        return sexo_usuario;
+    }
+
+    public void setSexo_usuario(String sexo_usuario) {
+        this.sexo_usuario = sexo_usuario;
+    }
+}
